9a32a2ca416dfe1951cd92c163c3a8e0c2542f63
vx-clutch <owestness@gmail.com>
2024-07-31 21:36:40 -0400

    c, py

--- cmd/vin/main.go
+++ cmd/vin/main.go
@@ -10,12 +10,19 @@ func main() {
 	lang := os.Args[1]
 	name := os.Args[2]
 	switch lang {
+	default:
+		fmt.Println("No lang found")
+		os.Exit(0)
 	case "go":
 		run("go", []string{"mod", "init", name})
-		os.MkdirAll(fmt.Sprintf("cmd/%v", name), 0666)
-		os.Mkdir("pkg", 0666)
-		os.MkdirAll("go-out/bin", 0666)
-		os.WriteFile(fmt.Sprintf("cmd/%v/main.go", name), []byte("package main\nfunc main() {\n\treturn\n}"), 0666)
+		os.MkdirAll(fmt.Sprintf("cmd/%v", name), 0755)
+		os.Mkdir("pkg", 0755)
+		os.MkdirAll("go-out/bin", 0755)
+		os.WriteFile(fmt.Sprintf("cmd/%v/main.go", name), []byte(get("go")), 0755)
+	case "c":
+		src("c", "c")
+	case "python":
+		src("python", "py")
 	}
 }
 
@@ -25,4 +32,22 @@ func run(command string, args []string) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func src(lang string, ext string) {
+	os.Mkdir("src", 0755)
+	os.MkdirAll(fmt.Sprintf("%v-out/bin", lang), 0755)
+	os.WriteFile(fmt.Sprintf("src/main.%v", ext), []byte(get(lang)), 0755)
+}
+
+func get(lang string) string {
+	switch lang {
+	case "go":
+		return "package main\n\nfunc main() {\n\treturn\n}"
+	case "c":
+		return "int main() {\n\treturn 0\n}"
+	case "python":
+		return "print('Hello, World!')"
+	}
+	return ""
 }
--- test/go.mod
+++
@@ -1,3 +0,0 @@
-module vin
-
-go 1.22.5
